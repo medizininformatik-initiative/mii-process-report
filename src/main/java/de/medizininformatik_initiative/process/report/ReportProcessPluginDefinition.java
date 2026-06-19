7dda862db29c06ae0d4c5c1234567372fa6d55c5
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2026-06-19 09:57:21 +0200

    release v2.0.0.0

--- src/main/java/de/medizininformatik_initiative/process/report/ReportProcessPluginDefinition.java
+++ src/main/java/de/medizininformatik_initiative/process/report/ReportProcessPluginDefinition.java
@@ -10,7 +10,7 @@ import dev.dsf.bpe.v2.ProcessPluginDefinition;
 public class ReportProcessPluginDefinition implements ProcessPluginDefinition
 {
 	public static final String VERSION = "2.0.0.0";
-	public static final LocalDate RELEASE_DATE = LocalDate.of(2025, 9, 12);
+	public static final LocalDate RELEASE_DATE = LocalDate.of(2026, 6, 19);
 
 	@Override
 	public String getName()
